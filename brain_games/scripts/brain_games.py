8c475c99d33e8f2d86cf904cf5997698574d8138
Саша Березин <attackofsnail@gmail.com>
2023-01-12 15:43:22 +0300

    add .

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import prompt
+from brain_games.scripts.cli import welcome_user
 
 def main():
 	print('Welcome to the Brain Games!')
-	name = prompt.string('May I have your name? ')
-	print('Hello,' + name + '!')
+	welcome_user()
 
 if __name__ == '__main__':
     main()
+
