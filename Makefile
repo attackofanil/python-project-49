a337b264d500c9720177e424da2dbfea65d70fe8
Саша Березин <attackofsnail@gmail.com>
2023-01-13 12:00:23 +0300

    add .

--- Makefile
+++ Makefile
@@ -15,3 +15,6 @@ package-install:
 
 prompt:
 	poetry add prompt
+
+lint:
+	poetry run flake8 brain_games
--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -1,8 +1,10 @@
 import prompt
 
+
 def welcome_user():
-	name = prompt.string('May I have your name? ')
-	print('Hello,' + name + '!')
+    name = prompt.string('May I have your name? ')
+    print('Hello,' + name + '!')
+
 
 if __name__ == '__main__':
-	welcome_user()
+    welcome_user()
