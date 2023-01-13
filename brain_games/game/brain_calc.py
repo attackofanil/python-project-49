f41cc505a687045dacc8c286841559e1c926aba7
Саша Березин <attackofsnail@gmail.com>
2023-01-13 20:14:12 +0300

    add .

---
+++ brain_games/game/brain_calc.py
@@ -0,0 +1,34 @@
+import random
+from random import randint
+from brain_games.scripts.cli import welcome_user
+
+
+
+def calc():
+  welcome_user()
+  print('What is the result of the expression?')
+  index = 0
+  while index < 3:
+    x = random.randint(50, 100)
+    y = random.randint(1, 50)
+    z = ['+', '-', '*']
+    end = z[randint(0, len(z)-1)]
+    print(f"Question: {x} {end} {y}")
+    if end == '+':
+      total = x + y
+    elif end == '-':
+      total = x - y
+    elif end == '*':
+      total = x * y
+    answer = int(input('Your answer: '))
+    result = 'Correct!'
+    if answer == total:
+      index += 1
+      print(result)
+    elif answer != total:
+      print(f"'{answer}' + is wrong answer ;(. Correct answer was {total}. Let's try again, {name}!")
+      break
+
+  if index == 3:
+    end = print(f"Congratulations, {name}!")
+    return end
